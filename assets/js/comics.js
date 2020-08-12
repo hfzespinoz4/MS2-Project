9cfcca9d8edbf79434ddef1eb097566f496c7e15
hfzespinoz4 <harvin.zuniga@gmail.com>
2020-08-12 21:45:18 +0200

    Fixing bugs con comics.js after validation

--- assets/js/comics.js
+++ assets/js/comics.js
@@ -1,29 +1,29 @@
-const comicsList = document.getElementById("comics-list");
+// const comicsList = document.getElementById("comics-list");
 const comicsNotification = document.getElementById("comic-notification");
 const comicSearchButton  = document.getElementById("comic-search");
-
 
 /*This function cleans the content (if exist) in the comic notification line 
 and also parse an error message to that line */
 function notificationClean (message){
     comicsNotification.innerText = "";
     comicsNotification.innerText = `${message}`;
-};
+}
 
 /* This function cleans the results (if exist) of a previous search.
    It search for all of "li" elements with a class ".comic-item" then
    remove all of them from its "ul" parent element */
 function resultsClean(){
     let comicsListItem = document.querySelectorAll(".comic-item");
-    for (i=0; li=comicsListItem[i]; i++){
+    let li;
+    for (let i=0; li=comicsListItem[i]; i++){
         li.parentNode.removeChild(li);
-    };
-};
+    }
+}
 
 /*This function get the information from MARVEL API */
 function comicSearch(){
     let comicName = document.getElementById("comic-name").value;
-    if (comicName != ""){fetch(`https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${comicName}&limit=20&ts=1&apikey=b2ecab084d4bcf291a6e227d03051c39&hash=87a58fd245d31989c4da906d2db06de7`)
+    if (comicName !== ""){fetch(`https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${comicName}&limit=20&ts=1&apikey=b2ecab084d4bcf291a6e227d03051c39&hash=87a58fd245d31989c4da906d2db06de7`)
         .then (response => {
             if (!response.ok){
                 notificationClean(`${response.status}`);
@@ -36,21 +36,14 @@ function comicSearch(){
         });
     } else {
     notificationClean("Please enter a comic name");
-    };
+    }
     return;
     
     // This function appends the api response to html elements
     function comicInfo(apiResponse){
         // First, we need to clean our screen
         notificationClean("");
-        comicItem = document.createElement("li");
-        // comicItem.className= "comic-item mt-2 mb-2";
-        // comicContainer = document.createElement("div");
-        // comicContainer.className = "comic-container";
-        // comicItem.appendChild(comicContainer)}
-
         apiResponse.data.results.map((comic, index) =>{
-            // comicsList.appendChild(comicItem); 
             comicsNotification.innerText = `There are ${index +1} results for your search`;
             $("#comics-list").append(`
             <li class="comic-item mt-2 mb-2">
@@ -68,9 +61,9 @@ function comicSearch(){
                 </div>
             </li>
             `);  
-        }) //Closing Map
+        }); //Closing Map
     } //Closing comicInfo function
-};//Closing comicSearch function
+}//Closing comicSearch function
 
 //Making a API Call when Search button is clicked
 comicSearchButton.addEventListener('click', function(event){
